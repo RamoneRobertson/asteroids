07e411c3bf833bd9145049cb249c2f23c03a74b1
Ramone Robertson <ramonerobertson10@gmail.com>
2026-04-13 19:23:46 -0500

    Capped the main game loop to 60 fps

--- main.py
+++ main.py
@@ -11,14 +11,19 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         log_state()
-        for event in pygame.event.get():
-            screen.fill("black")
-            pygame.display.flip()
 
+        for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        screen.fill("black")
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
